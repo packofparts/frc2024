f30bee68f31db932d1d12bbfca1642a60ecd8147
Powerlax <raovishruth@gmail.com>
2023-01-19 17:59:24 -0800

    Quick Fix: added AddRequirements(swervesubsystem) and cleaned up unused imports

--- src/main/java/frc/robot/commands/moveTo.java
+++ src/main/java/frc/robot/commands/moveTo.java
@@ -8,13 +8,9 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.SwerveModule;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class moveTo extends CommandBase {
@@ -44,6 +40,7 @@ public class moveTo extends CommandBase {
     yController = new PIDController(0.5, 0, 0);
     angleController = new PIDController(0.5, 0, 0);
     swerve = swervesub;
+    addRequirements(swerve);
 
     initPose = swerve.getRobotPose();
 
