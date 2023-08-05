d21b864aba10a8825492e63b6913d06fb1579ba0
ConfettiRaindrop <camilafigueira@outlook.com>
2023-08-05 15:09:57 -0700

    script to install required extensions

---
+++ tools/install_extensions.bat
@@ -0,0 +1,3 @@
+SET PoPCodeExtensions=eamodio.gitlens,donjayamanne.githistory,sonarsource.sonarlint-vscode,wpilibsuite.vscode-wpilib,wayou.vscode-todo-highlight
+
+for %%e in (%PoPCodeExtensions%) do code --install-extension %%e
