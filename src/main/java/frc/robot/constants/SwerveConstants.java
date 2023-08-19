f3fb47eb2feb906a938fac6a6b68cb7426702bf1
1294Laptop2 <s-tkoripella@lwsd.org>
2023-08-19 15:48:34 -0700

    Changed Encoder + Gear Ratio

--- src/main/java/frc/robot/constants/SwerveConstants.java
+++ src/main/java/frc/robot/constants/SwerveConstants.java
@@ -7,8 +7,7 @@ public class SwerveConstants {
   // Ratio for Mk2 Swerve Modules
   // Found https://www.chiefdelphi.com/t/2910-mk2-swerve-module-release/335077
   // and https://www.swervedrivespecialties.com/products/mk2-module-kit?variant=31141088821361
-  public static final double kRotGearRatio = 1 / 18;
-  public static final double kTransGearRatio = 1 / 8.33;
+  public static final double kTransGearRatio = 1 / 6.75;
 
   // Gear Conversions
   public static final double kDriveGearToMeters = (2 * Math.PI) * 2;
