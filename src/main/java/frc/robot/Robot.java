5bba87e3802fb82b563c09740f9b07686e062b97
Thanh Doan <tcdoan@gmail.com>
2023-08-27 15:54:58 -0700

    Starter code for the minimum mk4 robot.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,95 +6,40 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.FollowPath;
-import frc.robot.constants.AutoConstants;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command _autonomousCommand;
 
-  private RobotContainer _robotContainer;
-
-  public SendableChooser<Command> pathSelector = new SendableChooser<>();
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-    // and put our
-    // autonomous chooser on the dashboard.
-    _robotContainer = new RobotContainer();
-
-
-    pathSelector.addOption("NONE", null);
-    pathSelector.addOption("Station2PieceNOEVENT", new FollowPath(
-      _robotContainer.swerveSubsystem, 
-      AutoConstants.station2Piece,
-      AutoConstants.emptyMap
-    ));
-
-
-    SmartDashboard.putData("PP Autos", pathSelector);
+    /* To be defined */ 
   }
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow and SmartDashboard
-   * integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+    /* To be defined */ 
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    // _autonomousCommand = _robotContainer.getAutonomousCommand();
-
-    // if(pathSelector.getSelected() != null){
-    //   _autonomousCommand = pathSelector.getSelected();
-    // }
-
-    // // schedule the autonomous command (example)
-    // if (_autonomousCommand != null) {
-    //   _autonomousCommand.schedule();
-    // }
-    CANSparkMaxLowLevel.enableExternalUSBControl(true);
+  public void disabledInit() {
+    /* To be defined */ 
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void disabledPeriodic() {
+    /* To be defined */ 
+  }
+
+  @Override
+  public void autonomousInit() {
+    // Enable CANSparkMaxLowLevel.enableExternalUSBControl(true)
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    throw new UnsupportedOperationException();
+  }
 
   @Override
   public void teleopInit() {
@@ -102,32 +47,32 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (_autonomousCommand != null) {
-      _autonomousCommand.cancel();
-    }
   }
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() { 
+    /* To be defined */ 
+  }
 
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
     CANSparkMaxLowLevel.enableExternalUSBControl(true);
-    //_robotContainer.swerveSubsystem.setMotors(0, 0, 0);
+    // Enable _robotContainer.swerveSubsystem.setMotors(0, 0, 0)
   }
 
-  /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    // To be defined
+  }
 
-  /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    // To be defined
+  }
 
-  /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    // To be defined
+  }
 }
