ca0060cd9653e02b73f334a594dc88ea862f5565
Teja Koripella <tejakoripella@gmail.com>
2023-10-07 16:12:52 -0700

    added populated eventmap

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -16,7 +16,7 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class AutoConstants {
 
-    public static PathConstraints defaultSpeedConstraints = new PathConstraints(4, 3);
+    public static PathConstraints defaultSpeedConstraints = new PathConstraints(2, 1);
 
     public static PathPlannerTrajectory station2Piece = PathPlanner.loadPath("Station2Piece", defaultSpeedConstraints);
     public static PathPlannerTrajectory moveOneMeters = PathPlanner.loadPath("MoveOneMeters", defaultSpeedConstraints);
@@ -33,6 +33,7 @@ public class AutoConstants {
         
         if (doAction){  
 
+            // eventMap.put("ground_pickup_cone", );
 
 
         }else{
