f367bc0312ddae0935ff787f833f24a792f69070
SatishVaibhav <s-vsatish@lwsd.org>
2023-11-29 20:37:01 -0800

    Push Shooter subsystem

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.*;
+import edu.wpi.first.math.controller.PIDController;
+
 public class Shooter {
-    public Shooter(int frontMotorID, int backMotorID) {
+    TalonFX frontMotor;
+    TalonFX backMotor;
+    int _frontMotorID;
+    int _backMotorID;
+    PIDController _velocityPID;
+
+    public Shooter(int frontMotorID, int backMotorID, PIDController velocityPID) {
+        _frontMotorID = frontMotorID;
+        _backMotorID = backMotorID;
+        frontMotor = new TalonFX(frontMotorID);
+        backMotor = new TalonFX(backMotorID);
+        _velocityPID = velocityPID;
+    }
+
+    public void shoot() {
 
     }
 }
