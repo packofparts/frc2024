2c456128fff33ed6fdc496d7c3997f60a5f6ef47
Trinabh Gupta <trinabhgupta@Trinabhs-MacBook-Air.local>
2023-12-06 18:21:46 -0800

    added back and front motor

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,17 +8,24 @@ public class Shooter {
     TalonFX backMotor;
     int _frontMotorID;
     int _backMotorID;
-    PIDController _velocityPID;
+    PIDController _velocityPIDFront;
+    PIDController _velocityPIDBack;
 
-    public Shooter(int frontMotorID, int backMotorID, PIDController velocityPID) {
+
+    public Shooter(int frontMotorID, int backMotorID, PIDController velocityPIDFront,
+            PIDController velocityPIDBack) {
         _frontMotorID = frontMotorID;
         _backMotorID = backMotorID;
         frontMotor = new TalonFX(frontMotorID);
         backMotor = new TalonFX(backMotorID);
-        _velocityPID = velocityPID;
+        _velocityPIDFront = velocityPIDFront;
+        _velocityPIDBack = velocityPIDBack;
     }
 
-    public void shoot() {
-
+    public void shoot(double velocitySP) {
+        double PIDOutputFront = _velocityPIDFront.calculate(velocitySP);
+        frontMotor.set(PIDOutputFront);
+        double PIDOutputBack = _velocityPIDBack.calculate(velocitySP);
+        backMotor.set(PIDOutputBack);
     }
 }
